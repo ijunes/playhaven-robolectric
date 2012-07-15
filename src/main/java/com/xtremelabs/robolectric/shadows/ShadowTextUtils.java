077a52d7f23d166f34c61b31fa8cdc6c3fcafc47
Sam Stewart <sam@samstewartapps.com>
2012-07-15 12:03:11 -0700

    [add] htmlEncode() method to ShadowTextUtils

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowTextUtils.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowTextUtils.java
@@ -1,15 +1,18 @@
 package com.xtremelabs.robolectric.shadows;
 
+import java.util.Collection;
+
 import android.text.TextUtils;
+
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
+import com.xtremelabs.robolectric.internal.RealObject;
 import com.xtremelabs.robolectric.util.Join;
-
-import java.util.Collection;
 
 @SuppressWarnings({"UnusedDeclaration"})
 @Implements(TextUtils.class)
 public class ShadowTextUtils {
+	
     @Implementation
     public static CharSequence expandTemplate(CharSequence template,
                                               CharSequence... values) {
@@ -35,4 +38,34 @@ public class ShadowTextUtils {
     public static String join(CharSequence delimiter, Object[] tokens) {
         return Join.join((String) delimiter, tokens);
     }
+    
+    @Implementation
+    public static String htmlEncode(String s) {
+    	StringBuilder sb = new StringBuilder();
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            switch (c) {
+            case '<':
+                sb.append("&lt;"); //$NON-NLS-1$
+                break;
+            case '>':
+                sb.append("&gt;"); //$NON-NLS-1$
+                break;
+            case '&':
+                sb.append("&amp;"); //$NON-NLS-1$
+                break;
+            case '\'':
+                sb.append("&apos;"); //$NON-NLS-1$
+                break;
+            case '"':
+                sb.append("&quot;"); //$NON-NLS-1$
+                break;
+            default:
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
 }
